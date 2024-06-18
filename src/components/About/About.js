02c825ea850514fe189302740a597d806df8bd50
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-18 22:55:33 +0200

    about section default base

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './About.css';
+import chichaImage from '../../assets/about/colored-corn.png';
 
 const About = () => (
   <section className="info">
     <h2>CHICHA MORADA, C’EST QUOI ?</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-    <button className="learn-more-button">En savoir plus</button>
+    <div className="content">
+      <div className="text">
+        <h3>TITRE</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <button className="learn-more-button">En savoir plus</button>
+      </div>
+      <div className="image-container">
+        <img src={chichaImage} alt="Chicha Morada" />
+      </div>
+    </div>
   </section>
 );
 
