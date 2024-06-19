fa9e1552e635b6910434f47f130916f5025ec7a1
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-19 15:08:36 +0200

    main to be continued

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,7 +6,7 @@ const Main = () => (
     <div className="hero-content">
       <h1>TITRE</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <button className="discover-button">Découvrir</button>
+      <button className="discover-button">En savoir plus</button>
     </div>
   </section>
 );
