df1a2db145f00779399ade0933be809bcc037e86
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-19 19:07:33 +0200

    gallery done

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Gallery.css';
-import image1 from '../../assets/gallery/image1.png'; // Adjust the path as needed
-import image2 from '../../assets/gallery/image3.jpg'; // Adjust the path as needed
-import image3 from '../../assets/gallery/image2.jpg'; // Adjust the path as needed
-import alpaca from '../../assets/gallery/image4.png'; // Adjust the path as needed
+import firstImage from '../../assets/gallery/two-chicha-morada-bottles.png';
+import secondImage from '../../assets/gallery/maracuya-glass.png';
+import thirdImage from '../../assets/gallery/chicha-morada-in-the-clouds.png';
+import alpaca from '../../assets/gallery/alpaca.png';
 
 const Gallery = () => (
   <section className="gallery">
     <div className="gallery-images">
-      <img src={image1} alt="Image 1" className="gallery-image" />
-      <img src={image2} alt="Image 2" className="gallery-image" />
-      <img src={image3} alt="Image 3" className="gallery-image" />
+      <div className="small-images">
+        <img src={firstImage} alt="Image 1" className="gallery-image" />
+        <img src={secondImage} alt="Image 2" className="gallery-image" />
+      </div>
+      <img src={thirdImage} alt="Image 3" className="gallery-image large-image" />
     </div>
     <div className="gallery-alpaca">
       <img src={alpaca} alt="Alpaca" />
