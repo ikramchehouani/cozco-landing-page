7afacaa23c34e6b35a92e23b70400179428538d2
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-19 14:31:28 +0200

    base footer put

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,30 @@ import './Footer.css';
 
 const Footer = () => (
   <footer className="footer">
-    <p>&copy; 2024 Cozco. All rights reserved.</p>
+    <div className="footer-content">
+      <div className="footer-logo">
+        <img src="/cozco-logo.png" alt="Cozco Chicha Morada" />
+      </div>
+      <div className="footer-links">
+        <ul>
+          <li>Accueil</li>
+          <li>Produits</li>
+        </ul>
+        <ul>
+          <li>Où nous trouver ?</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className="footer-contact">
+        <h3>CONTACTEZ-NOUS</h3>
+        <p>Jorge Mellado - Président</p>
+        <p>Téléphone : 06 64 31 68 83</p>
+        <p>Mail : contact.cozco@gmail.com</p>
+      </div>
+    </div>
+    <div className="footer-bottom">
+      <p>&copy;Cozco 2024 - Mention légales - Politique de confidentialité</p>
+    </div>
   </footer>
 );
 
