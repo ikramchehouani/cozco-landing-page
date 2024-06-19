65948e4f5472bcfecbe4e0a28b0a28598f92c9a2
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-19 12:09:38 +0200

    history base

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './History.css';
+import peopleImage from '../../assets/history/peruvian-girls-with-lama.jpg';
 
 const History = () => (
-  <section className="history">
-    <h2>LE TEMPS ET L’HISTOIRE</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+  <section className="info">
+    <h2>Le temps et l'histoire</h2>
+    <div className="content">
+      <div className="text">
+        <h3>TITRE</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <button className="learn-more-button">En savoir plus</button>
+      </div>
+      <div className="image-container">
+        <img src={peopleImage} alt="Peruvian girls with a lama" />
+      </div>
+    </div>
   </section>
 );
 
