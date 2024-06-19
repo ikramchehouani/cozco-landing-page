9c32ff76c33a7d352207e2be935f32d434406bd0
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-19 20:08:06 +0200

    navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
-const Navbar = () => (
-  <header className="header">
-    <div className="logo">
-      <img src="/cozco-logo.png" alt="Cozco Chicha Morada" />
-    </div>
-    <div className="nav-button-container">
-      <nav>
-        <ul>
-          <li>Produits</li>
-          <li>Où nous trouver ?</li>
-        </ul>
-      </nav>
-      <button className="order-button">PASSER COMMANDE</button>
-    </div>
-  </header>
-);
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className={`header ${menuOpen ? 'active' : ''}`}>
+      <div className="logo">
+        <img src="/cozco-logo.png" alt="Cozco Chicha Morada logo" />
+      </div>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="nav-button-container">
+        <nav>
+          <ul>
+            <li>Produits</li>
+            <li>Où nous trouver ?</li>
+          </ul>
+        </nav>
+        <button className="order-button">Passer commande</button>
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
