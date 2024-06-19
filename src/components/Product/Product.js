9b05dc9e18775c784e15dffd8f76c50353c7dd8e
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-19 11:58:05 +0200

    product base

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Product.css';
+import productImage from '../../assets/product/chicha-morada-product.png';
 
 const Product = () => (
   <section className="products">
-    <h2>NOS PRODUITS</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-    <button className="products-button">Découvrez nos produits</button>
+    <div className="products-image">
+      <img src={productImage} alt="Chicha Morada" />
+    </div>
+    <div className="products-content">
+      <h2>Nos produits</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <button className="products-button">Découvrez nos produits</button>
+    </div>
   </section>
 );
 
