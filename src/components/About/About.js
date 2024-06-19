e1b751aefc0e7b65c6fe5be6e9e981d9e0425f7d
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-19 16:37:36 +0200

    about section, added splashes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,8 +7,8 @@ const About = () => (
     <h2>Chicha Morada, c'est quoi ?</h2>
     <div className="content">
       <div className="text">
-        <h3>TITRE</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h3>Une boisson emblématique 100% péruvienne</h3>
+        <p>Nous produisons localement la chicha morada, une boisson artisanale très appréciée au Pérou. Faite à base de maïs violet, de fruits, de cannelle et de clou de girofle.</p>
         <button className="learn-more-button">En savoir plus</button>
       </div>
       <div className="image-container">
