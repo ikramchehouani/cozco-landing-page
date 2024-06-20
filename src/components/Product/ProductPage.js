7e7a26d76ea5922c45fbb2cfe80c1dfa0a852fb1
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-20 19:48:39 +0200

    titles added for product page and restaurant

--- src/components/Product/ProductPage.js
+++ src/components/Product/ProductPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ProductChoices from './ProductChoices';
 import Order from '../Order/Order';
+import './ProductPage.css';
+import decorativeImage from '../../assets/string-yellow-for-titles.png';
 
 const ProductPage = () => (
   <div className="product-page">
+    <section className="title-section">
+      <div className="decorative-line">
+        <img src={decorativeImage} alt="Decorative" className="decorative-image" />
+        <h2 className="section-title">NOS BOISSONS</h2>
+        <img src={decorativeImage} alt="Decorative" className="decorative-image" />
+      </div>
+    </section>
     <ProductChoices />
     <Order />
   </div>
--- src/components/Restaurant/RestaurantPage.js
+++ src/components/Restaurant/RestaurantPage.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Map from './Map';
 import Order from '../Order/Order';
 import Restaurant from './Restaurant';
+import decorativeImage from '../../assets/string-yellow-for-titles.png';
 
 const RestaurantPage = () => (
   <div className="product-page">
+    <section className="title-section">
+      <div className="decorative-line">
+        <img src={decorativeImage} alt="Decorative" className="decorative-image" />
+        <h2 className="section-title">Où nous trouver ?</h2>
+        <img src={decorativeImage} alt="Decorative" className="decorative-image" />
+      </div>
+    </section>
     <Map />
     <Restaurant />
     <Order />
