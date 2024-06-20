0374f1f095b6d61f8f388b61ded09c75044a8f93
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-20 14:10:14 +0200

    main version f

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,8 +5,9 @@ const Main = () => (
   <section className="hero">
     <div className="hero-content">
       <h1>Le soleil du Pérou en bouteille</h1>
-      <p>Fraîchement préparée en région parisienne, notre chicha morada Cozco est une invitation à découvrir les saveurs authentiques du Pérou dans chaque gorgée. Partagez avec nous un moment de pur plaisir et de gourmandise !</p>
-      <button className="discover-button">En savoir plus</button>
+      <p>
+        Fraîchement préparée en région parisienne, notre Chicha Morada de Cozco est une invitation à découvrir les saveurs authentiques du Pérou dans chaque gorgée. Partagez avec nous un moment de pur plaisir et de gourmandise !
+      </p>
     </div>
   </section>
 );
