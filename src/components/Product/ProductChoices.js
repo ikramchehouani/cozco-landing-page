6ac55cc9ca9b7186711829e2dc783ad506b4d261
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-06-20 19:26:25 +0200

    product adjusted for responsive

--- src/components/Product/ProductChoices.js
+++ src/components/Product/ProductChoices.js
@@ -4,34 +4,42 @@ import chichaMoradaImage from '../../assets/product/chicha-morada-product.png';
 import maracuyaImage from '../../assets/product/maracuya-product.png';
 
 const ProductChoices = () => (
-    <section className="product-choices">
-      <div className="product-row">
-        <div className="product-item">
-          <div className="product-image">
-            <img src={chichaMoradaImage} alt="Chicha Morada" />
-          </div>
-          <div className="product-description" style={{ backgroundColor: '#960061', color: '#fff' }}>
-            <h2>CHICHA MORADA</h2>
-            <p>La Chicha Morada est une boisson populaire et familiale typiquement péruvienne. Elle est fabriquée à partir d'une variété de maïs violet foncé qui est intensivement cultivée dans la cordillère des Andes au Pérou.</p>
-            <p>Composition : maïs violet, pomme, ananas, cannelle, clous de girofle, sucre de canne</p>
-            <p>Prix : 1 caissette de 16 bouteilles 1L de CHICHA MORADA = 79,20€ (hors taxes)</p>
-          </div>
+  <section className="product-choices">
+    <div className="product-row">
+      <div className="product-item chicha-morada">
+        <div className="product-image">
+          <img src={chichaMoradaImage} alt="Chicha Morada" />
+        </div>
+        <div className="product-description">
+          <h2>CHICHA MORADA</h2>
+          <p>La Chicha Morada est une boisson populaire et familiale typiquement péruvienne. Elle est fabriquée à partir d'une variété de maïs violet foncé qui est intensivement cultivée dans la cordillère des Andes au Pérou.</p>
+          <p><strong>Composition :</strong> maïs violet, pomme, ananas, cannelle, clous de girofle, sucre de canne</p>
+          <p><strong>Prix :</strong></p>
+          <ul>
+            <li>1 caissette de 16 bouteilles d’1L = 79,20€ HT</li>
+            <li>1 caissette de 32 bouteilles de 50cl = 95,20€ HT</li>
+          </ul>
         </div>
       </div>
-      <div className="product-row">
-        <div className="product-item">
-          <div className="product-description" style={{ backgroundColor: '#FABA05', color: '#960061' }}>
-            <h2>MARACUYA</h2>
-            <p>Le jus de Maracuya, ou fruit de la passion, est une boisson très populaire au Pérou et dans le reste de l'Amérique latine. C'est une boisson rafraîchissante, à la fois douce et acide.</p>
-            <p>Composition : fruit de la passion, citron vert, sucre de canne</p>
-            <p>Prix : 1 caissette de 16 bouteilles 1L de MARACUYA = 84,20€ (hors taxes)</p>
-          </div>
-          <div className="product-image">
-            <img src={maracuyaImage} alt="Maracuya" />
-          </div>
+    </div>
+    <div className="product-row">
+      <div className="product-item maracuya">
+        <div className="product-description">
+          <h2>MARACUYA</h2>
+          <p>Le jus de Maracuya, ou fruit de la passion, est une boisson très populaire au Pérou et dans le reste de l'Amérique latine. C'est une boisson rafraîchissante, à la fois douce et acide.</p>
+          <p><strong>Composition :</strong> fruit de la passion, citron vert, sucre de canne</p>
+          <p><strong>Prix :</strong></p>
+          <ul>
+            <li>1 caissette de 16 bouteilles d’1L = 84,80€ HT</li>
+            <li>1 caissette de 32 bouteilles de 50cl = 95,20€ HT</li>
+          </ul>
+        </div>
+        <div className="product-image">
+          <img src={maracuyaImage} alt="Maracuya" />
         </div>
       </div>
-    </section>
-  );
-  
+    </div>
+  </section>
+);
+
 export default ProductChoices;
